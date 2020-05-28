172cd47504d4c103423117bb8a194579b0bcf50d
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-27 21:00:00 -0700

    added information to seed files for department and roles

--- db/schema.sql
+++ db/schema.sql
@@ -10,9 +10,9 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER PRIMARY KEY,
     title VARCHAR(30) NOT NULL, 
-    salary DECIMAL 
+    salary DECIMAL NOT NULL,
     department_id INTEGER UNSIGNED, 
-    CONSTRAINT fk_party FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL 
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL 
 )
 
 CREATE TABLE employee (
@@ -20,9 +20,9 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30), 
     role_id INTEGER UNSIGNED, 
-    CONSTRAINT fk_party FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     manager_id INTEGER UNSIGNED,
-    CONSTRAINT fk_party FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 )
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,21 @@
 INSERT INTO department (name)
 VALUES 
+("IT"),
+("Sales & Marketing"),
+("Finance & Accounting"),
+("Operations"),
 
 INSERT INTO role (title, salary, department_id)
 VALUES
+("Full Stack Developer", ),
+("Software Engineer", ),
+("Accountant", ), 
+("Finanical Analyst", )
+("Marketing Coordindator", ), 
+("Sales Lead", ),
+("Project Manager", )
+("Operations Manager", )
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
