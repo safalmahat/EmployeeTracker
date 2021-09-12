ee73c75448d16145c150cd2b41821aede34efc61
RESHAM\user <safal1mahat@gmail.com>
2021-09-12 19:43:31 +0545

    package updated

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const mysql = require('mysql2')
 // import inquirer 
 const inquirer = require('inquirer'); 
-// import console.table
 const cTable = require('console.table'); 
 
 require('dotenv').config()
